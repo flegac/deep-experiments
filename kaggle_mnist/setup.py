c5b3d04be3c4b45a1cff12b65fc7d707710641bd
flegac <florent.legacgmail.com>
2018-11-30 20:27:14 +0100

    fix for CPU training

---
+++ kaggle_mnist/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='kaggle_mnist',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='florent',
+    author_email='',
+    description='',
+    install_requires=['keras', 'pandas', 'scikit-learn', 'tensorflow', 'matplotlib']
+)
