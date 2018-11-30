baf1ad7ca717a4aa39ec97bb651f059ab4b649fe
flegac <florent.legac@gmail.com>
2018-11-30 22:29:04 +0100

    feat: random image from numpy

--- data_preparation/preparation/plot_image.py
+++ data_preparation/preparation/plot_image.py
@@ -6,7 +6,16 @@ x, y = np.meshgrid(np.linspace(-2, 2, 200), np.linspace(-2, 2, 200))
 x, y = x - x.mean(), y - y.mean()
 z = x * np.exp(-x ** 2 - y ** 2)
 
+zz = np.array(z)
+print(zz.shape)
+
 # Plot the grid
 plt.imshow(z)
 plt.gray()
+plt.show()
+
+img = np.random.rand(256, 256, 3)
+
+plt.imshow(img)
+plt.colormaps()
 plt.show()
