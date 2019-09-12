86def4c5b2d8360f31fb294a8995a9ce93860c35
flegac <florent.legac@gmail.com>
2019-09-12 09:39:39 +0200

    feat: clustering

--- image-tools/image_dataframe/database.py
+++ image-tools/image_dataframe/database.py
@@ -7,7 +7,6 @@ import numpy as np
 import pandas as pd
 from joblib import dump, load
 from more_itertools import flatten
-from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.cluster import KMeans
 from sklearn.pipeline import Pipeline
 
@@ -32,9 +31,9 @@ def tile_dataframe(df: pd.DataFrame, tiler: GridTiler):
         x1 = row[1]['x1']
         x2 = row[1]['x2']
         y = row[1]['y']
-        # f = lambda _: _
-        # x_img1 = f(cv2.imread(x1))
-        # x_img2 = f(cv2.imread(x2))
+        f = lambda _: _
+        x_img1 = f(cv2.imread(x1))
+        x_img2 = f(cv2.imread(x2))
 
         y_img = cv2.imread(y)
 
@@ -42,8 +41,8 @@ def tile_dataframe(df: pd.DataFrame, tiler: GridTiler):
             (
                 x1, x2, y,
                 box.top, box.bottom, box.left, box.right,
-                # *np.average(np.hstack((box.cut(x_img1), box.cut(x_img2))), axis=(0, 1)),
-                # *np.std(np.hstack((box.cut(x_img1), box.cut(x_img2))), axis=(0, 1)),
+                *np.average(np.hstack((box.cut(x_img1), box.cut(x_img2))), axis=(0, 1)),
+                *np.std(np.hstack((box.cut(x_img1), box.cut(x_img2))), axis=(0, 1)),
                 (box.cut(y_img) == 1).sum()
             )
             for box in tiler.tiles(y_img.shape)
@@ -57,27 +56,10 @@ def tile_dataframe(df: pd.DataFrame, tiler: GridTiler):
     return pd.DataFrame(data=rows, columns=[
         'x1', 'x2', 'y',
         'top', 'bottom', 'left', 'right',
-        # 'avg_b', 'avg_g', 'avg_r',
-        # 'std_b', 'std_g', 'std_r',
+        'avg_b', 'avg_g', 'avg_r',
+        'std_b', 'std_g', 'std_r',
         'tagged'
     ])
-
-
-class ComputeColorStats(BaseEstimator, TransformerMixin):
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, X, y=None, **fit_params):
-        features = np.empty(shape=(len(X), 6), dtype=float)
-        for i, x in enumerate(X):
-            features[i] = (
-                *np.average(x, axis=(0, 1)),
-                *np.std(x, axis=(0, 1)),
-            )
-        return features
-
-    def fit_transform(self, X, y=None, **fit_params):
-        return self.transform(X, y, **fit_params)
 
 
 class Clustering(object):
@@ -88,7 +70,6 @@ class Clustering(object):
             self.pipeline = load(path)
         else:
             self.pipeline = Pipeline(steps=[
-                ('stats', ComputeColorStats()),
                 ('clustering', KMeans(n_clusters=n_clusters, n_init=20)),
             ])
             self.fit(df)
@@ -114,13 +95,19 @@ class Clustering(object):
         return np.hstack((box.cut(x1), box.cut(x2)))
 
 
-tile_size = 64
-df = tile_dataframe(
-    df=df_from_path('../../feat-detection/detection/images'),
-    tiler=GridTiler(tile_size=tile_size, stride=32)
-)
-
 os.makedirs('data', exist_ok=True)
+dataset_path = 'data/dataste.csv'
+tile_size = 64
+stride = 32
+
+if os.path.exists(dataset_path):
+    df = pd.read_csv(dataset_path)
+else:
+    df = tile_dataframe(
+        df=df_from_path('../../feat-detection/detection/images'),
+        tiler=GridTiler(tile_size=tile_size, stride=stride)
+    )
+    df.to_csv(dataset_path)
 
 model = Clustering('data/model.joblib', df)
 model.save()
