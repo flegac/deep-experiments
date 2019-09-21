0bcd5a0c7385a95f39622467e7079267a85f805f
flegac <florent.legac@gmail.com>
2019-09-21 17:48:29 +0200

    feat: File menu open image/data

--- image-editor/editor_gui/app.py
+++ image-editor/editor_gui/app.py
@@ -1,7 +1,7 @@
 import os
 
 from editor_gui.config import EDITOR_CONFIG
-from editor_gui.event_bus import PROJECT_OPEN_BUS, IMAGE_OPEN_BUS
+from editor_gui.event_bus import PROJECT_OPEN_BUS, IMAGE_OPEN_BUS, DATASET_OPEN_BUS
 from editor_gui.utils.ui_utils import dir_selection, file_selection
 from editor_gui.window import Win
 from editor_plugins.image_filter.plugin import ImageFilterPlugin
@@ -9,10 +9,16 @@ from editor_plugins.morphology.plugin import MorphologyPlugin
 from editor_plugins.tiling.plugin import TilingPlugin
 
 
-def open_editor():
+def open_image():
     paths = file_selection()
     name, _ = os.path.splitext(os.path.basename(paths[0]))
     IMAGE_OPEN_BUS.on_next((name, paths[0]))
+
+
+def open_data():
+    paths = file_selection()
+    name, _ = os.path.splitext(os.path.basename(paths[0]))
+    DATASET_OPEN_BUS.on_next((name, paths[0]))
 
 
 if __name__ == '__main__':
@@ -30,9 +36,13 @@ if __name__ == '__main__':
 
     win.config_menu({
         'File': {
-            'New': lambda: IMAGE_OPEN_BUS.on_next(('---', None)),
+            'New image': lambda: IMAGE_OPEN_BUS.on_next(('---', None)),
+            'Open image': open_image,
+
+            'New data': lambda: DATASET_OPEN_BUS.on_next(('---', None)),
+            'Open data': open_data,
+
             'Open project': lambda: PROJECT_OPEN_BUS.on_next(dir_selection()),
-            'Open image': open_editor,
 
             'Exit': win.destroy
 
