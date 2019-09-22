9cb30607c5120ae8617767ea1dd1c184d576bc18
flegac <florent.legac@gmail.com>
2019-09-22 22:35:15 +0200

    feat: basic paint controller

--- image-editor/editor_gui/editors/image/image_view.py
+++ image-editor/editor_gui/editors/image/image_view.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 
+import cv2
 from PIL import ImageTk, Image
 from rx import operators
 from rx.subject import Subject
 
+from editor_api.data.buffer import Buffer
 from editor_api.data.data_core import DataSource
 from editor_api.data.data_utils import DataUtils
 from editor_core.viewport import ViewportOperator
@@ -11,24 +13,23 @@ from editor_gui.utils.hidden_scrollbar import HiddenScrollbar
 
 
 class ImageView(tk.Frame):
-    ZOOM_SPEED = 0.75
     MAX_REDRAW_PER_SEC = 24
 
     def __init__(self, master: tk.Widget):
         tk.Frame.__init__(self, master)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
 
+        # redraw bus
         self.redraw_bus = Subject()
         self.redraw_bus.pipe(
             operators.throttle_first(1. / ImageView.MAX_REDRAW_PER_SEC)
         ).subscribe(on_next=lambda _: self.redraw_canvas())
 
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
         # canvas creation
         self.canvas = tk.Canvas(self, width=600, height=400)
         self.canvas.grid(row=0, column=0, sticky='nsew')
-
-        # Vertical and horizontal scrollbars for canvas
         vbar = HiddenScrollbar(self, orient='vertical', command=self.canvas.yview)
         vbar.grid(row=0, column=1, sticky='ns')
         hbar = HiddenScrollbar(self, orient='horizontal', command=self.canvas.xview)
@@ -37,19 +38,90 @@ class ImageView(tk.Frame):
         self.canvas.configure(yscrollcommand=vbar.set, xscrollcommand=hbar.set)
 
         # image
-        self.data = DataUtils.empty_source.get_buffer()
+        self.data = None
         self.image = None
         self.image_id = None
-        self.viewport = ViewportOperator(self.viewport_size)
 
-        # help
-        self.debug_id = self.canvas.create_text(5, 60, anchor='nw', text='')
+        self.viewport_controller = ViewportController(self.canvas, self.redraw_bus)
+        self.viewport_controller.bind()
+
+        self.paint_controller = PaintController(self)
 
         # Bind events to the Canvas
         self.canvas.bind('<Configure>', lambda event: self.redraw_canvas())
+        self.bind_all('k', lambda _: self.viewport_controller.unbind_canvas())
+        self.bind_all('a', self.paint_controller.paint)
 
+    def get_data(self, data_source: DataSource = None) -> Buffer:
+        if data_source is not None:
+            self.data = data_source.get_buffer()
+        return self.data
+
+    def on_source_change(self, source: DataSource):
+        self.get_data(source)
+        self.redraw_bus.on_next(source)
+
+    def redraw_canvas(self):
+        if self.image_id:
+            self.canvas.delete(self.image_id)
+
+        raw_data = self.get_data()
+        data = self.viewport_controller.viewport.apply(raw_data)
+
+        self.image = ImageTk.PhotoImage(image=Image.fromarray(data))
+        self.image_id = self.canvas.create_image((0, 0), anchor=tk.NW, image=self.image)
+        self.canvas.lower(self.image_id)  # set it into background
+
+
+class PaintController(object):
+    def __init__(self, view: ImageView):
+        self.view = view
+        self.brush_size = 8
+        self.brush_func = cv2.rectangle
+        self.color = (0, 255, 0)
+        self.thickness = 1
+
+    def paint(self, event=None):
+        img_x, img_y = self.view.viewport_controller.mouse_image_coords()
+        self.view.data = cv2.rectangle(
+            self.view.data,
+            (img_x - self.brush_size, img_y - self.brush_size),
+            (img_x + self.brush_size, img_y + self.brush_size),
+            color=(0, 255, 0),
+            thickness=1
+        )
+        self.view.redraw_bus.on_next(None)
+
+
+class ViewportController(object):
+    ZOOM_SPEED = 0.75
+    EVENTS = [
+        '<ButtonPress-1>',
+        '<B1-Motion>',
+        '<Button-4>',
+        '<Button-5>',
+        '<MouseWheel>',
+        '<Key>'
+    ]
+
+    def __init__(self, canvas, redraw_bus: Subject):
+        self.canvas = canvas
+        self.redraw_bus = redraw_bus
+
+        self.viewport = ViewportOperator(self.viewport_size)
         self.mouse_x = None
         self.mouse_y = None
+
+    def mouse_image_coords(self):
+        root = self.canvas.master
+        z = self.viewport.zoom_factor
+        mouse_x = int((root.winfo_pointerx() - root.winfo_rootx()) / z)
+        mouse_y = int((root.winfo_pointery() - root.winfo_rooty()) / z)
+        img_x = mouse_x + self.viewport.x
+        img_y = mouse_y + self.viewport.y
+        return img_x, img_y
+
+    def bind(self):
         self.canvas.bind('<ButtonPress-1>', self.start_move)
         self.canvas.bind('<B1-Motion>', self.move)
         # linux scroll
@@ -57,6 +129,17 @@ class ImageView(tk.Frame):
         self.canvas.bind('<Button-5>', self.zoom)
         # windows scroll
         self.canvas.bind('<MouseWheel>', self.zoom)
+
+    def unbind_canvas(self):
+        if self.canvas is not None:
+            for _ in self.EVENTS:
+                self.canvas.unbind(_)
+            self.canvas = None
+
+    def viewport_size(self):
+        w = self.canvas.winfo_width()
+        h = self.canvas.winfo_height()
+        return w, h
 
     def start_move(self, event):
         self.mouse_x = event.x
@@ -69,7 +152,7 @@ class ImageView(tk.Frame):
         self.viewport.move(dx, dy)
         self.mouse_x = event.x
         self.mouse_y = event.y
-        self.redraw_bus.on_next({})
+        self.redraw_bus.on_next(None)
 
     def zoom(self, event):
         # Respond to Linux (event.num) or Windows (event.delta) wheel event
@@ -77,38 +160,12 @@ class ImageView(tk.Frame):
             self.viewport.zoom(self.ZOOM_SPEED)
         if event.num == 4 or event.delta == 120:
             self.viewport.zoom(1 / self.ZOOM_SPEED)
-        self.redraw_bus.on_next({})
-
-    def read_data(self, data_source: DataSource = None):
-        if data_source is not None:
-            self.data = data_source.get_buffer()
-        return self.data
-
-    def viewport_size(self):
-        w = self.canvas.winfo_width()
-        h = self.canvas.winfo_height()
-        return w, h
-
-    def on_source_change(self, source: DataSource):
-        self.read_data(source)
         self.redraw_bus.on_next(None)
-
-    def redraw_canvas(self):
-
-        if self.image_id:
-            self.canvas.delete(self.image_id)
-
-        raw_data = self.read_data()
-        data = self.viewport.apply(raw_data)
-
-        self.image = ImageTk.PhotoImage(image=Image.fromarray(data))
-        self.image_id = self.canvas.create_image((0, 0), anchor=tk.NW, image=self.image)
-        self.canvas.lower(self.image_id)  # set it into background
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     editor = ImageView(root)
-    editor.on_source_change(DataUtils.empty_source)
+    editor.on_source_change(DataUtils.random_source)
     editor.pack(fill='both', expand=True)
     root.mainloop()
