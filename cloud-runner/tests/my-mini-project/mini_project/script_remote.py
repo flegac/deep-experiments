f92e02bcb14b532b4a7c70e0499edde093be4036
flegac <florent.legac@gmail.com>
2019-09-24 08:41:47 +0200

    fix: using pathlib

--- cloud-runner/tests/my-mini-project/mini_project/script_remote.py
+++ cloud-runner/tests/my-mini-project/mini_project/script_remote.py
@@ -1,9 +1,11 @@
 import math
+from pathlib import Path
 
-from cloud_runner.script_runner import ScriptRunner
 from cloud_runner.cluster.google_cluster import GoogleCluster
 from cloud_runner.cluster.google_utils import cpu_config
+from cloud_runner.script_runner import ScriptRunner
 
+root = Path.cwd().parent.parent.parent.parent
 ScriptRunner(
     creation_sleep_time=60,
     script_relative_path='my-mini-project/mini_project/script_local.py',
@@ -14,9 +16,9 @@ ScriptRunner(
         'learning_rate': math.pow(10, -(i + 2))
     } for i in range(1)],
     to_deploy=[
-        '../../my-mini-project',
-        '../../../../surili-lib',
-        '../../../../stream-lib',
+        str(Path('my-mini-project').parent.parent),
+        str(root / 'surili-lib'),
+        str(root / 'stream-lib'),
     ]
 ).run_with(GoogleCluster(
     name='test-cluster',
